e3abbb25d61df4fe227cc65e0d60f58d713481f8
Spectralite <spectralphazetime@gmail.com>
2024-08-11 22:36:44 +0100

    syncing updated vimrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,86 +1,80 @@
-------------------------------
---|[***** CP-STUFF *****]|
-------------------------------
-vim.opt.number = true
-vim.opt.tabstop = 4      
-vim.opt.shiftwidth = 4    
-vim.opt.expandtab = true   
-vim.opt.smartindent = true  
-vim.opt.clipboard = 'unnamedplus'
-vim.keymap.set('i', '{', '{}<Left>', { noremap = true })
-vim.keymap.set('i', '{<CR>', '{<CR>}<Esc>O', { noremap = true })
-vim.keymap.set('i', '{{', '{', { noremap = true })
-vim.keymap.set('i', '{}', '{}', { noremap = true })
-
-vim.keymap.set('i', '[', '[]<Left>', { noremap = true })
-vim.keymap.set('i', '[<CR>', '[<CR>]<Esc>O', { noremap = true })
-vim.keymap.set('i', '[[', '[', { noremap = true })
-vim.keymap.set('i', '[]', '[]', { noremap = true })
-
-vim.keymap.set('i', '(', '()<Left>', { noremap = true })
-vim.keymap.set('i', '(<CR>', '(<CR>)<Esc>O', { noremap = true })
-vim.keymap.set('i', '((', '(', { noremap = true })
-vim.keymap.set('i', '()', '()', { noremap = true })
-
-vim.keymap.set('i', '""', '"', { noremap = true })
-------------------------------------------
------|[***** NON-CP Begins Here *****]|
-------------------------------------------
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.keymap.set('i', '$', function()
-  vim.api.nvim_put({'$$'}, 'c', true, true)
-  vim.api.nvim_input('<Left>')
+--[CP - SETUP]
+vim.opt.bg ="light"
+vim.opt.nu = true
+vim.opt.rnu= true
+vim.opt.cul = true
+vim.opt.ts = 4
+vim.opt.sw = 4
+vim.opt.et = true
+vim.opt.si = true
+vim.opt.udf = true
+vim.opt.so = 10
+vim.opt.ic = true
+vim.opt.wrap = false
+vim.opt.cb = "unnamedplus"
+vim.keymap.set("i", "(", "()<left>", { noremap = true })
+vim.keymap.set("i", "{", "{}<left>", { noremap = true })
+vim.keymap.set("i", "[", "[]<left>", { noremap = true })
+vim.keymap.set("i", "\"", "\"\"<left>", { noremap = true })
+vim.keymap.set("i", "'", "''<left>", { noremap = true })
+--[CP - END]
+vim.g.mapleader = " "
+vim.keymap.set("i", "$", function()
+	vim.api.nvim_put({ "$$" }, "c", true, true)
+	vim.api.nvim_input("<Left>")
 end, { noremap = true, silent = true })
- -- Header configuration
-local function insert_cpp_header()
-    local date = os.date("%d/%m/%Y")
-    local time = os.date("%H:%M:%S")
-    local header = {
-        string.format('// %s - %s', date, time),
-    }
-    vim.api.nvim_buf_set_lines(0, 0, 0, false, header)
-end
-vim.api.nvim_create_autocmd({"BufNewFile"}, {
-    pattern = "*.cpp",
-    callback = function()
-        insert_cpp_header()
-    end,
+vim.api.nvim_create_autocmd("BufNewFile", {
+	pattern = "*.cpp",
+	callback = function()
+		local header = string.format("// %s - %s", os.date("%d/%m/%Y"), os.date("%H:%M:%S"))
+		vim.api.nvim_buf_set_lines(0, 0, 0, false, { header })
+	end,
 })
-vim.cmd[[
-" Use Tab to expand and jump through snippets
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-" Use Shift-Tab to jump backwards through snippets
-imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+vim.api.nvim_set_keymap("n", "<leader>ps", ":vsplit | wincmd l | terminal pwsh<CR>", { noremap = true, silent = true })
+vim.api.nvim_exec(
+    [[
+  augroup CPPSnippets
+    autocmd!
+    autocmd FileType cpp inoremap <buffer> cf <C-R>=luaeval('CPPSnippet()')<CR>
+  augroup END
+]],
+    false
+)
+function _G.CPPSnippet()
+    local snippet = [[
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <utility>
+#include <string> 
+#include <set>
+using namespace std;
+using str = string; 
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+#define mp make_pair
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); 
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); 
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+int main(){ 
+    setIO();
+}
 ]]
--- Map <leader>ps to open PowerShell in a horizontal split
-vim.api.nvim_set_keymap('n', '<leader>ps', ':vsplit | terminal pwsh<CR>', { noremap = true, silent = true })
--- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end 
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup({
-   {
-    'L3MON4D3/LuaSnip',
-    lazy = true,
-    event = 'InsertEnter',
-    build = function()
-      if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
-        return
-      end
-      return 'make install_jsregexp'
-    end,
-    config = function()
-      require("luasnip").config.set_config({
-        enable_autosnippets = true,
-        store_selection_keys = "<Tab>",
-      })
-      require("luasnip.loaders.from_lua").load({paths = "C:/Users/Eyobell/AppData/Local/nvim/LuaSnip"})
-    end,
-  },
-} )
+vim.api.nvim_put(vim.split(snippet, "\n"), "l", true, true)
+	return ""
+end
+
